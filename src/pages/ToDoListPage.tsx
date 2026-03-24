3788af86045623c250d3b151b9ea1d30037be555
Marina <litvinms94@gmail.com>
2026-03-24 12:21:40 +0500

    Add new comps

---
+++ src/pages/ToDoListPage.tsx
@@ -0,0 +1,16 @@
+import { Form } from "../components/Form/Form";
+import { Header } from "../components/Header/Header";
+import { ToDoList } from "../components/ToDoList/ToDoList";
+
+
+export const ToDoListPage = () => {
+    return (
+        <>
+            <Header />
+
+            <Form />
+            
+            <ToDoList />
+        </>
+    );
+};
